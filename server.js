7ce14b09852d588f49fe00a31faad4d7becd2438
Ignacio Palma <naxopalma@gmail.com>
2017-03-17 17:40:26 -0300

    deploy

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ var client = new pg.Client(connectionString);
 client.connect(function (err) {
   if (err) throw err;
 
-  client.query("CREATE TABLE categories(id SERIAL PRIMARY KEY, name VARCHAR(40) not null)"); 
-  client.query("CREATE TABLE menuitems(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, description VARCHAR(40) not null, price INTEGER not null, category_id INTEGER REFERENCES categories(id))")         
-  client.query("CREATE TABLE orders(id SERIAL PRIMARY KEY, customer VARCHAR(40) not null, totalPrice INTEGER not null)"); 
-  client.query("CREATE TABLE suborders(id SERIAL PRIMARY KEY, subtotalprice INTEGER not null, quantity INTEGER not null, totalPrice INTEGER not null, id_orders INTEGER REFERENCES orders(id), id_menuitems INTEGER REFERENCES menuitems(id))");
+  // client.query("CREATE TABLE categories(id SERIAL PRIMARY KEY, name VARCHAR(40) not null)"); 
+  // client.query("CREATE TABLE menuitems(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, description VARCHAR(40) not null, price INTEGER not null, category_id INTEGER REFERENCES categories(id))")         
+  // client.query("CREATE TABLE orders(id SERIAL PRIMARY KEY, customer VARCHAR(40) not null, totalPrice INTEGER not null)"); 
+  // client.query("CREATE TABLE suborders(id SERIAL PRIMARY KEY, subtotalprice INTEGER not null, quantity INTEGER not null, totalPrice INTEGER not null, id_orders INTEGER REFERENCES orders(id), id_menuitems INTEGER REFERENCES menuitems(id))");
 
-  client.query("INSERT INTO categories(name) VALUES('burgers'), ('dinner'), ('breakfast'), ('drinks')");                  
-  client.query("INSERT INTO menuitems(name, description, price, category_id) VALUES('bigmac', 'the biggest burger', 122, 1), ('nuggets', 'little nuggets', 232, 3), ('fries', 'good fries', 23, 2)");                  
+  // client.query("INSERT INTO categories(name) VALUES('burgers'), ('dinner'), ('breakfast'), ('drinks')");                  
+  // client.query("INSERT INTO menuitems(name, description, price, category_id) VALUES('bigmac', 'the biggest burger', 122, 1), ('nuggets', 'little nuggets', 232, 3), ('fries', 'good fries', 23, 2)");                  
 });
 
 app.use(bodyParser.json());
@@ -41,6 +41,10 @@ app.listen(app.get('port'), function() {
 
 
 // Routes
+app.get('/', function(req, res, next) {  
+  res.sendfile('client/index.html');                  
+});
+
 app.get('/categories', function(req, res, next) {  
   client.query("SELECT * FROM categories", function(err, result) {   
     res.send(result);
