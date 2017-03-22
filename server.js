a5f1b20835428f4a8e0a26e0302ff93f97ccbc94
Preston Moore <caroham29@gmail.com>
2017-03-21 18:16:24 -0700

    Updated price type to numeric, total now holds decimals

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bcrypt = require('bcrypt-nodejs');
 var bodyParser = require('body-parser');
 var routes = require('./routes');
 
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/mypjdb';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pjmydb';
 
 var routes = require('./routes');
 
@@ -20,35 +20,36 @@ client.connect(function (err) {
 //   if (err) throw err;
 
 
-//   client.query("CREATE TABLE \
-//                   categories( \
-//                     id SERIAL PRIMARY KEY, \
-//                     name VARCHAR(40) not null)");
-//
-//   client.query("CREATE TABLE \
-//                   menuitems( \
-//                     id SERIAL PRIMARY KEY, \
-//                     name VARCHAR(40) not null, \
-//                     description VARCHAR(40) not null, \
-//                     price INTEGER not null, \
-//                     category_id INTEGER REFERENCES categories(id))");
-//
-//   client.query("CREATE TABLE \
-//                   orders( \
-//                     id SERIAL PRIMARY KEY, \
-//                     customer VARCHAR(40) not null, \
-//                     totalprice INTEGER not null, \
-//                     complete boolean default false)");
-//
-//   client.query("CREATE TABLE \
-//                   suborders( \
-//                     id SERIAL PRIMARY KEY, \
-//                     description VARCHAR(40) not null, \
-//                     subtotalprice INTEGER not null, \
-//                     quantity INTEGER not null, \
-//                     id_orders INTEGER REFERENCES orders(id), \
-//                     id_menuitems INTEGER REFERENCES menuitems(id))");
-//
+
+  // client.query("CREATE TABLE \
+  //                 categories( \
+  //                   id SERIAL PRIMARY KEY, \
+  //                   name VARCHAR(40) not null)");
+
+  // client.query("CREATE TABLE \
+  //                 menuitems( \
+  //                   id SERIAL PRIMARY KEY, \
+  //                   name VARCHAR(40) not null, \
+  //                   description VARCHAR(40) not null, \
+  //                   price NUMERIC(4,2) not null, \
+  //                   category_id INTEGER REFERENCES categories(id))");
+
+  // client.query("CREATE TABLE \
+  //                 orders( \
+  //                   id SERIAL PRIMARY KEY, \
+  //                   customer VARCHAR(40) not null, \
+  //                   totalprice NUMERIC(4,2) not null, \
+  //                   complete boolean default false)");
+
+  // client.query("CREATE TABLE \
+  //                 suborders( \
+  //                   id SERIAL PRIMARY KEY, \
+  //                   description VARCHAR(40) not null, \
+  //                   subtotalprice NUMERIC(4,2) not null, \
+  //                   quantity INTEGER not null, \
+  //                   id_orders INTEGER REFERENCES orders(id), \
+  //                   id_menuitems INTEGER REFERENCES menuitems(id))");
+
 //   client.query("INSERT INTO \
 //                   categories(name) \
 //                     VALUES('breakfast'), \
@@ -56,25 +57,25 @@ client.connect(function (err) {
 //                           ('dinner'), \
 //                           ('desert'), \
 //                           ('drinks')");
-//
+
 // client.query("INSERT INTO \
 //                   menuitems(name, description, price, category_id) \
-//                     VALUES('Walker Texas Brisket', 'Texas sized burger in walker sauce', 12, 3), \
-//                       ('Roundhouse Kick Burger', 'Roundhouse kick to the gut', 12, 3), \
-//                       ('The Delta Four', 'Cheese Pizza', 14, 3), \
-//                       ('Kickin Grits and Taters', 'Grits and seasoned taters', 9, 1), \
-//                       ('Magnus Stack', 'Large stack of pancakes', 9, 1), \
-//                       ('Hearty Oats and Toast', 'Oatmeal served with toast', 9, 1)");
-//
+//                     VALUES('Walker Texas Brisket', 'Texas sized burger in walker sauce', 12.99, 3), \
+//                       ('Roundhouse Kick Burger', 'Roundhouse kick to the gut', 12.99, 3), \
+//                       ('The Delta Four', 'Cheese Pizza', 14.99, 3), \
+//                       ('Kickin Grits and Taters', 'Grits and seasoned taters', 9.99, 1), \
+//                       ('Magnus Stack', 'Large stack of pancakes', 9.99, 1), \
+//                       ('Hearty Oats and Toast', 'Oatmeal served with toast', 9.99, 1)");
+
 // client.query("INSERT INTO \
 //                   menuitems(name, description, price, category_id) \
-//                     VALUES('Grilled Cheese Sandwich', 'Tasty grilled cheese sandwich', 7, 2), \
-//                       ('Philly Cheese Sandwich', 'Philly cheese style sandwich', 12, 2), \
-//                       ('Walker Kickin Chicken Salad', 'Grilled chicken salad', 9, 2), \
-//                       ('Bucket O Oreos', 'Oreos with a tall glass of milk', 5, 3), \
-//                       ('Red Bearded Velvet Cake', 'Red Velvelt Cake', 5, 3), \
-//                       ('Mango Spritzer', 'Mango and orange juice in champagne', 7, 4)");
-//
+//                     VALUES('Grilled Cheese Sandwich', 'Tasty grilled cheese sandwich', 7.99, 2), \
+//                       ('Philly Cheese Sandwich', 'Philly cheese style sandwich', 12.99, 2), \
+//                       ('Walker Kickin Chicken Salad', 'Grilled chicken salad', 9.99, 2), \
+//                       ('Bucket O Oreos', 'Oreos with a tall glass of milk', 5.99, 3), \
+//                       ('Red Bearded Velvet Cake', 'Red Velvelt Cake', 5.99, 3), \
+//                       ('Mango Spritzer', 'Mango and orange juice in champagne', 7.99, 4)");
+
 
 
 });
