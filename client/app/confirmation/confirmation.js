3810b48956b51a135a56fa835972f66293a88023
Preston Moore <caroham29@gmail.com>
2017-03-25 02:10:36 -0700

    Email confirmation, optional name on confirmation

--- client/app/confirmation/confirmation.js
+++ client/app/confirmation/confirmation.js
@@ -1,26 +1,37 @@
 angular.module('confirmation', ['services'])
-  .controller('confirmationController', function($http, $scope, menuitemsService){
+  .controller('confirmationController', function($http, $scope, menuitemsService) {
     getOrders();
     $scope.data = menuitemsService.getChosenList();
     $scope.lastOrder;
-    $scope.sendEmail = function(data) {
-      console.log(typeof data)
+    $scope.sendEmail = function(data, name) {
       var data = data;
+      var name = name;
       $scope.data = {};
-      $http.post('/email', JSON.stringify({email : data})).then(function(response){
+      $http.post('/valid',JSON.stringify({email : data})).then(function(response){
+        if(response.data) {
+          alert("You're savings are on the way! Check your email " + data + " for details.");
+          $http.post('/email', JSON.stringify({email : data, name : name}))
+            .then(function(response){
+          });
+          setTimeout(function(){window.location = "http://localhost:5000/";}, 10000);
+        } else {
+          alert("Please enter a valid email address for coupons and spnecial offers!");
+        }
       });
     };
     //iterate through orders 
     function getOrders() {
+      setTimeout(function(){window.location = "http://localhost:5000/";}, 30000);
       $http({
         method: 'GET',
-        url: '/deeporders'
+        url: '/getmax'
       }).then(function successCallback(response) {
-       // console.log('DEEP ORDERS', response)
-        $scope.orders = response.items;
-        $scope.neworder = response.data;
-        $scope.neworder = $scope.neworder[$scope.neworder.length-1];
-       //console.log($scope.neworder)
+        // console.log('DEEP ORDERS', response);
+        var data = response.data;
+        $scope.orderid = data[0];
+        $scope.customer = data[1];
+        $scope.totalprice = data[2];
+        $scope.neworder = data[3];
       }, function errorCallback(response) {
         console.log('Error getting orders', response);
       });
@@ -42,4 +53,3 @@ angular.module('confirmation', ['services'])
       }
     }
   });
-
