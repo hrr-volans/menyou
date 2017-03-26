20d78136f59c5ddee07f94489bb502d6219aa7cf
Ignacio Palma <naxopalma@gmail.com>
2017-03-26 17:32:22 -0300

    seed DB in production

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var url = require('url');
 // var helper = require('sendgrid').mail;
 // var sg = require('sendgrid')(process.env.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);
 
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pjmydb';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/menyoudb';
 
 
 var routes = require('./routes');
@@ -28,65 +28,62 @@ client.connect(function (err) {
 
   if (err) throw err;
 
-//   if (err) throw err;
 
+  client.query("CREATE TABLE \
+                  categories( \
+                    id SERIAL PRIMARY KEY, \
+                    name VARCHAR(40) not null)");
 
-//   client.query("CREATE TABLE \
-//                   categories( \
-//                     id SERIAL PRIMARY KEY, \
-//                     name VARCHAR(40) not null)");
+  client.query("CREATE TABLE \
+                  menuitems( \
+                    id SERIAL PRIMARY KEY, \
+                    name VARCHAR(40) not null, \
+                    description VARCHAR(40) not null, \
+                    price NUMERIC(6,2) not null, \
+                    category_id INTEGER REFERENCES categories(id))");
 
-//   client.query("CREATE TABLE \
-//                   menuitems( \
-//                     id SERIAL PRIMARY KEY, \
-//                     name VARCHAR(40) not null, \
-//                     description VARCHAR(40) not null, \
-//                     price NUMERIC(6,2) not null, \
-//                     category_id INTEGER REFERENCES categories(id))");
+  client.query("CREATE TABLE \
+                  orders( \
+                    id SERIAL PRIMARY KEY, \
+                    customer VARCHAR(40) not null, \
+                    totalprice NUMERIC(6,2) not null, \
+                    complete boolean default false)");
 
-//   client.query("CREATE TABLE \
-//                   orders( \
-//                     id SERIAL PRIMARY KEY, \
-//                     customer VARCHAR(40) not null, \
-//                     totalprice NUMERIC(6,2) not null, \
-//                     complete boolean default false)");
+  client.query("CREATE TABLE \
+                  suborders( \
+                    id SERIAL PRIMARY KEY, \
+                    description VARCHAR(40) not null, \
+                    subtotalprice NUMERIC(6,2) not null, \
+                    quantity INTEGER not null, \
+                    id_orders INTEGER REFERENCES orders(id), \
+                    id_menuitems INTEGER REFERENCES menuitems(id))");
 
-//   client.query("CREATE TABLE \
-//                   suborders( \
-//                     id SERIAL PRIMARY KEY, \
-//                     description VARCHAR(40) not null, \
-//                     subtotalprice NUMERIC(6,2) not null, \
-//                     quantity INTEGER not null, \
-//                     id_orders INTEGER REFERENCES orders(id), \
-//                     id_menuitems INTEGER REFERENCES menuitems(id))");
+  client.query("INSERT INTO \
+                  categories(name) \
+                    VALUES('breakfast'), \
+                          ('lunch'), \
+                          ('dinner'), \
+                          ('desert'), \
+                          ('drinks')");
 
-//   client.query("INSERT INTO \
-//                   categories(name) \
-//                     VALUES('breakfast'), \
-//                           ('lunch'), \
-//                           ('dinner'), \
-//                           ('desert'), \
-//                           ('drinks')");
+client.query("INSERT INTO \
+                  menuitems(name, description, price, category_id) \
+                    VALUES('Walker Texas Brisket', 'Texas sized burger in walker sauce', 12.99, 3), \
+                      ('Roundhouse Kick Burger', 'Roundhouse kick to the gut', 12.99, 3), \
+                      ('The Delta Four-Cheese Pizza', 'Baked by Chuck staring at it for 1 min', 14.99, 3), \
+                      ('Kickin Grits and Taters', 'Grits and seasoned taters', 9.99, 1), \
+                      ('Magnus Stack', 'Large stack of pancakes', 9.99, 1), \
+                      ('Hearty Oats and Toast', 'Oatmeal served with toast', 9.99, 1)");
 
-// client.query("INSERT INTO \
-//                   menuitems(name, description, price, category_id) \
-//                     VALUES('Walker Texas Brisket', 'Texas sized burger in walker sauce', 12.99, 3), \
-//                       ('Roundhouse Kick Burger', 'Roundhouse kick to the gut', 12.99, 3), \
-//                       ('The Delta Four-Cheese Pizza', 'Baked by Chuck staring at it for 1 min', 14.99, 3), \
-//                       ('Kickin Grits and Taters', 'Grits and seasoned taters', 9.99, 1), \
-//                       ('Magnus Stack', 'Large stack of pancakes', 9.99, 1), \
-//                       ('Hearty Oats and Toast', 'Oatmeal served with toast', 9.99, 1)");
+client.query("INSERT INTO \
+                  menuitems(name, description, price, category_id) \
+                    VALUES('Grilled Cheese Sandwich', 'Tasty grilled cheese sandwich', 7.99, 2), \
+                      ('Philly Cheese Sandwich', 'Philly cheese style sandwich', 12.99, 2), \
+                      ('Walker Kickin Chicken Salad', 'Grilled chicken salad', 9.99, 2), \
+                      ('Bucket O Oreos', 'Oreos with a tall glass of milk', 5.99, 3), \
+                      ('Red Bearded Velvet Cake', 'Red Velvelt Cake', 5.99, 3), \
+                      ('Mango Spritzer', 'Mango and orange juice in champagne', 7.99, 4)");
 
-// client.query("INSERT INTO \
-//                   menuitems(name, description, price, category_id) \
-//                     VALUES('Grilled Cheese Sandwich', 'Tasty grilled cheese sandwich', 7.99, 2), \
-//                       ('Philly Cheese Sandwich', 'Philly cheese style sandwich', 12.99, 2), \
-//                       ('Walker Kickin Chicken Salad', 'Grilled chicken salad', 9.99, 2), \
-//                       ('Bucket O Oreos', 'Oreos with a tall glass of milk', 5.99, 3), \
-//                       ('Red Bearded Velvet Cake', 'Red Velvelt Cake', 5.99, 3), \
-//                       ('Mango Spritzer', 'Mango and orange juice in champagne', 7.99, 4)");
-
-// });
 });
 
 app.use(bodyParser.json());
