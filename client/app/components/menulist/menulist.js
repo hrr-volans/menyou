7bd8d64bc4afdee6fe36ab800350a2b5aab223dd
Ignacio Palma <naxopalma@gmail.com>
2017-03-27 01:58:28 -0300

    hacked menuitems 13

--- client/app/components/menulist/menulist.js
+++ client/app/components/menulist/menulist.js
@@ -8,7 +8,7 @@ angular.module('menulist', ['services'])
       console.log('new res', result)
       $scope.data = categoriesService.getMenuItemsInCurrentCategory; 
       console.log('scope data: ', $scope.data);
-      $scope.category = result.data.categoryName;     
+      $scope.category = result.getCurrentCategory;     
     })
     categoriesService.newSetMenuByCategories();
 
